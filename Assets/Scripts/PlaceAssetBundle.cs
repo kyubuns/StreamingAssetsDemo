2b83822bfe7ee2b7e012a973f0e5fb3a468c0bd7
kyubuns <kyubuns@gmail.com>
2019-08-03 16:58:50 +0900

    feat: AssetBundleを設置

---
+++ Assets/Scripts/PlaceAssetBundle.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace StreamingAssetsDemo
+{
+    public class PlaceAssetBundle : MonoBehaviour
+    {
+        [SerializeField] private string assetBundleName = default;
+        [SerializeField] private string assetName = default;
+
+        public void Start()
+        {
+            var path = Path.Combine(Application.streamingAssetsPath, "AssetBundles", assetBundleName);
+            var bytes = File.ReadAllBytes(path);
+            var assetBundle = AssetBundle.LoadFromMemory(bytes);
+            var prefab = assetBundle.LoadAsset(assetName);
+            Instantiate(prefab, transform);
+        }
+    }
+}
