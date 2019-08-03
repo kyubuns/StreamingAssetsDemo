494fa6e670ae722d45aa3659fab6027598165a3b
kyubuns <kyubuns@gmail.com>
2019-08-03 16:51:50 +0900

    feat: AssetBundleビルドスクリプト

---
+++ Assets/Scripts/Editor/BuildAssetBundle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace StreamingAssetsDemo.Editor
+{
+    public static class BuildAssetBundle
+    {
+        [MenuItem("Build/AssetBundle")]
+        public static void Run()
+        {
+            var outputPath = Path.Combine(Application.dataPath, "StreamingAssets", "AssetBundles");
+            Debug.Log($"Build AssetBundle to {outputPath}");
+
+            const BuildAssetBundleOptions options = BuildAssetBundleOptions.StrictMode;
+            var manifest = BuildPipeline.BuildAssetBundles(outputPath, options, EditorUserBuildSettings.activeBuildTarget);
+            if (manifest == null) throw new Exception("BuildAssetBundle Error");
+
+            Debug.Log("Build AssetBundle Complete!");
+        }
+    }
+}
